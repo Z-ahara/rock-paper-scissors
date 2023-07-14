3a930addc394208dc6cab248c0188d6259b4c460
Z-ahara <zaharambell@gmail.com>
2023-07-14 15:01:55 -0400

    write psuedo code for script

---
+++ script.js
@@ -0,0 +1,19 @@
+// game is playing against the computer
+
+// let playerSelection = user input prompt()
+// let computerSelection = random choice
+
+/*
+function getComputerChoice {
+    need another function that randomizes between choices rock, paper, or scissors
+}
+
+// plays a single round of rock paper scissors
+function playRound(playerSelection, computerSelection)
+    return string that declares winner
+
+function game
+    use for loop to call playRound function 5 times
+
+console.log() to display results each round & winner at the end
+*/
