5e586e1c9006f68b28088d9a4c56eaf95bc701d3
Z-ahara <zaharambell@gmail.com>
2023-07-17 13:31:13 -0400

    Create RPS function that plays single round RPS, create prompt for user input, use code to randomize computer choice

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-// game is playing against the computer
-
-// let playerSelection = user input prompt()
-// let computerSelection = random choice
-
-/*
-function getComputerChoice {
-    need another function that randomizes between choices rock, paper, or scissors
+// math.floor rounds a number down to nearest integer, math.random returns a floating-point random number >= 0 and less than 1
+// randomizes between rock, paper, and scissors based on randomized numbers
+function getComputerChoice () {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber == 0) {
+        return "rock"
+    } else if (randomNumber == 1) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
 }
 
-// plays a single round of rock paper scissors
-function playRound(playerSelection, computerSelection)
-    return string that declares winner
+// goes thru each rock, paper, scissors scenarios (find way to shorten)
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == "rock" & computerSelection == "paper") {
+        return "You lose! Paper Beats Rock"
+    } else if (playerSelection.toLowerCase() == "rock" & computerSelection == "rock") {
+        return "It's a tie!"
+    } else if (playerSelection.toLowerCase() == "rock" & computerSelection == "scissors") {
+        return "Computer loses! Rock beats scissors"
+    } else if (playerSelection.toLowerCase() == "paper" & computerSelection == "rock") {
+        return "Computer loses! Paper beats rock"
+    } else if (playerSelection.toLowerCase() == "paper" & computerSelection == "paper") {
+        return "It's a tie!"
+    } else if (playerSelection.toLowerCase() == "paper" & computerSelection == "scissors") {
+        return "You lose! Scissors beats paper"
+    } else if (playerSelection.toLowerCase() == "scissors" & computerSelection == "rock") {
+        return "You lose! Rock beats scissors"
+    } else if (playerSelection.toLowerCase() == "scissors" & computerSelection == "scissors") {
+        return "It's a tie!"
+    } else if (playerSelection.toLowerCase() == "scissors" & computerSelection == "paper") {
+        return "Computer loses! Scissors beats paper"
+    }
+}
 
-function game
-    use for loop to call playRound function 5 times
+const playerSelection = prompt("Rock, paper, or scissors?")
+const computerSelection = getComputerChoice()
 
-console.log() to display results each round & winner at the end
-*/
+console.log(playRound(playerSelection, computerSelection))
